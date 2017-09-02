995baab89935506dd12f2dd5706f5b31a2a14743
Dominic <dv@cat-ia.de>
2017-09-02 10:14:31 +0200

    update header and footer

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<!-- Im Browser nicht sichtbare Inhalte -->
+<head>
+
+	<!-- Zeichensatz einstellen -->
+	<meta charset="utf-8">
+
+	<!-- Viewport für mobile konfigurieren -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Wordpress Workshop</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Abel|Roboto+Slab" rel="stylesheet">
+	<link rel="stylesheet" href="dist/css/app.css" type="text/css" media="all" />
+
+	<?php wp_head(); ?>
+
+</head>
+
+<!-- Im Browser sichtbare Inhalte -->
+<body>
+
+<!-- Logo -->
+<a href="#" class="logo">
+	<span>DV</span>
+</a>
+<!-- Logo ENDE -->
+
+<!-- Navigation -->
+<nav class="mod--navigation">
+	<ul class="list--reset main--navigation">
+		<li>
+			<a href="#" title="Home">Home</a>
+		</li>
+		<li>
+			<a href="#" title="Portrait">Portrait</a>
+		</li>
+		<li>
+			<a href="#" title="Konzerte">Lost Place</a>
+		</li>
+		<li>
+			<a href="#" title="Über mich">Über mich</a>
+		</li>
+	</ul>
+</nav>
+<!-- Navigation ENDE -->
