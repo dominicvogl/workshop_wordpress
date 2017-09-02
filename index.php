bdec319e0643d5e3ed575eac02968f5166c1951b
Dominic <dv@cat-ia.de>
2017-09-02 13:27:26 +0200

    add basic loop for content

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
 
          <div class="mod--slider">
             <div>
-               <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/lost_place-09.jpg" alt="preview"/>
+               <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/lost_place-09.jpg"
+                    alt="preview"/>
             </div>
             <div>
-               <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/lost_place-10.jpg" alt="preview"/>
+               <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/lost_place-10.jpg"
+                    alt="preview"/>
             </div>
             <div>
-               <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/lost_place-13.jpg" alt="preview"/>
+               <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/lost_place-13.jpg"
+                    alt="preview"/>
             </div>
          </div>
 
@@ -88,17 +91,6 @@
 
                </li>
 
-               <li class="feed--item">
-
-                  <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/lost_place-11.jpg"
-                       alt="Lost Place">
-
-                  <div class="item--meta">
-                     <span class="tag">Lost Place</span>
-                  </div>
-
-               </li>
-
             </ul>
          </div>
       </div>
@@ -110,75 +102,34 @@
 
       <ul class="list--reset blogroll row">
 
-         <!-- Blogpost -->
-         <li class="column col-6">
-            <article>
-               <div class="inner-content">
-                  <h3>Mein erster Blogpsot</h3>
-                  <b>03.04.2017</b> // Autor: <strong>Dominic Vogl</strong>
-                  <p>Das ist eine tolle Einleitung für meinen ersten Blogbeitrag... Lorem ipsum dolor sit amet,
-                     consectetur adipiscing elit.</p>
-                  <a class="button" href="#">weiterlesen</a>
-               </div>
-            </article>
-         </li>
-         <!-- Blogpost ENDE -->
-
-         <!-- Blogpost -->
-         <li class="column col-6">
-            <article>
-               <div class="inner-content">
-                  <h3>Mein erster Blogpsot</h3>
-                  <b>03.04.2017</b> // Autor: <strong>Dominic Vogl</strong>
-                  <p>Das ist eine tolle Einleitung für meinen ersten Blogbeitrag... Lorem ipsum dolor sit amet,
-                     consectetur adipiscing elit.</p>
-                  <a class="button" href="#">weiterlesen</a>
-               </div>
-            </article>
-         </li>
-         <!-- Blogpost ENDE -->
-
-         <!-- Blogpost -->
-         <li class="column col-4">
-            <article>
-               <div class="inner-content">
-                  <h3>Mein erster Blogpsot</h3>
-                  <b>03.04.2017</b> // Autor: <strong>Dominic Vogl</strong>
-                  <p>Das ist eine tolle Einleitung für meinen ersten Blogbeitrag... Lorem ipsum dolor sit amet,
-                     consectetur adipiscing elit.</p>
-                  <a class="button" href="#">weiterlesen</a>
-               </div>
-            </article>
-         </li>
-         <!-- Blogpost ENDE -->
-
-         <!-- Blogpost -->
-         <li class="column col-4">
-            <article>
-               <div class="inner-content">
-                  <h3>Mein erster Blogpsot</h3>
-                  <b>03.04.2017</b> // Autor: <strong>Dominic Vogl</strong>
-                  <p>Das ist eine tolle Einleitung für meinen ersten Blogbeitrag... Lorem ipsum dolor sit amet,
-                     consectetur adipiscing elit.</p>
-                  <a class="button" href="#">weiterlesen</a>
-               </div>
-            </article>
-         </li>
-         <!-- Blogpost ENDE -->
-
-         <!-- Blogpost -->
-         <li class="column col-4">
-            <article>
-               <div class="inner-content">
-                  <h3>Mein erster Blogpsot</h3>
-                  <b>03.04.2017</b> // Autor: <strong>Dominic Vogl</strong>
-                  <p>Das ist eine tolle Einleitung für meinen ersten Blogbeitrag... Lorem ipsum dolor sit amet,
-                     consectetur adipiscing elit.</p>
-                  <a class="button" href="#">weiterlesen</a>
-               </div>
-            </article>
-         </li>
-         <!-- Blogpost ENDE -->
+		  <?php
+		  $count = 0;
+		  if ( have_posts() ) {
+			  while ( have_posts() ) {
+				  the_post();
+				  $columns = 'column col-4';
+				  if ( $count < 2 ) {
+					  $columns = 'column col-6';
+				  }
+				  ?>
+                 <!-- Blogpost -->
+                 <li class="<?php echo $columns; ?>">
+                    <article>
+                       <div class="inner-content">
+                          <h3><?php the_title(); ?></h3>
+                          <b><?php the_time( 'd. m. Y' ); ?></b> // Autor: <strong><?php the_author(); ?></strong>
+                          <div class="content"><?php the_excerpt(); ?></div>
+                          <a class="button" href="<?php the_permalink(); ?>">weiterlesen</a>
+                       </div>
+                    </article>
+                 </li>
+                 <!-- Blogpost ENDE -->
+				  <?php
+				  $count ++;
+			  }
+			  wp_reset_postdata();
+		  }
+		  ?>
 
       </ul>
 
